b910879e5bab4eade4a26810ec060fb0ccdcd0e7
elnexreal <40328573+elnexreal@users.noreply.github.com>
2025-01-23 02:09:33 -0300

    jsoned the jsonny

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <cpr/cpr.h>
 #include <fmt/core.h>
+#include <nlohmann/json.hpp>
 
 int main(int argc, char **argv) {
 	cpr::Response r = cpr::Get(
 		cpr::Url{"https://catfact.ninja/fact"}
 	);
 
-	fmt::println("Status: {} {}", r.status_code, r.status_line);
-	fmt::println("Body: {}", r.text);
+	nlohmann::json json = nlohmann::json::parse(r.text);
+
+	fmt::println("Status: {} {}", r.status_code, r.reason);
+	fmt::println("Cat fact: {}", json["fact"].get<std::string>());
 
 	return 0;
 }
